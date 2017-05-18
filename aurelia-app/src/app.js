9ab00bb722135292df6b5a01ccd7ee668cf80d2b
Daniel Rosendorf <daniel.rosendorf@zuehlke.com>
2017-05-18 11:38:25 +0200

    Step 2: Result

--- aurelia-app/src/app.js
+++ aurelia-app/src/app.js
@@ -1,5 +1,10 @@
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
-  }
+    configureRouter(config, router) {
+        this.router = router;
+        config.title = 'Aurelia';
+        config.map([
+            { route: ['', 'todo'], name: 'Todos', moduleId: 'todo/index' },
+            { route: 'todo/add', name: 'Add new todo', moduleId: 'todo/add' }
+        ]);
+    }
 }
