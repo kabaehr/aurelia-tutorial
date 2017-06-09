03d17ff6fffe84690d2e689146952e647b654abd
kaba <Katharina.Baehr@zuehlke.com>
2017-06-09 22:30:33 +0200

    Step 4: Result

--- aurelia-app/src/todo/add.ts
+++ aurelia-app/src/todo/add.ts
@@ -1,8 +1,27 @@
 import { autoinject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
 import { TodoService } from './todo-service';
+import { Todo } from './todo';
 
 @autoinject()
 export class Add {
-    constructor(private todoService: TodoService) {
+    description: string;
+    priority: string;
+    deadline: Date;
+
+    constructor(private todoService: TodoService, private router: Router) {
+        this.initializeProperties();
+    }
+
+    initializeProperties() {
+        this.description = '';
+        this.priority = 'Medium';
+        this.deadline = new Date();
+    }
+
+    addTodo() {
+        let todo = new Todo(this.description, this.deadline, this.priority);
+        this.todoService.todos.push(todo);
+        this.router.navigateToRoute('Todos');
     }
 }
--- aurelia-app/src/todo/index.ts
+++ aurelia-app/src/todo/index.ts
@@ -1,8 +1,12 @@
-import { autoinject} from 'aurelia-framework';
+import { autoinject } from 'aurelia-framework';
 import { TodoService } from './todo-service';
 
 @autoinject()
 export class Index {
     constructor(private todoService: TodoService) {
+    }
+
+    get todos() {
+        return this.todoService.todos;
     }
 }
