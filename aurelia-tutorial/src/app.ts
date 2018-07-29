5990784788f6e34164a7bc6a4064f4567211dfc4
kaba <>
2018-07-29 22:36:09 +0200

    Step 2: Result

--- aurelia-tutorial/src/app.ts
+++ aurelia-tutorial/src/app.ts
@@ -1,3 +1,14 @@
+import { RouterConfiguration, Router } from 'aurelia-router';
+
 export class App {
-  message = 'Hello World!';
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router): void {
+    this.router = router;
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'todo'], name: 'todo-list', moduleId: 'routes/todo/todo' },
+      { route: 'todo/add', name: 'add-todo', moduleId: 'routes/todo/add' }
+    ]);
+  }
 }
